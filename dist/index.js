3eff3b66056bc66bd49ab068c83a7dfee9b9f3d3
Agboolafeyiemi <agboolafeyikemi93@gmail.com>
2020-02-21 23:48:38 +0100

    message

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,17 @@
 //add  dynamic date
 window.onload = addDynamicDate;
- 
+
 function addDynamicDate() {
-  const year = new Date().getFullYear()
-  const copyright = document.getElementById("myList")
-  .appendChild(document.createTextNode(` .© Copyright ${year}`));
+  const year = new Date().getFullYear();
+  let node = document.createElement("LI");
+  let nodeText = document.createTextNode(` .© Copyright ${year}`);
+  node.appendChild(nodeText);
+  document.getElementById("myList").appendChild(node);
 };
 
- // Reset Form Input
-document.getElementById('handleSubmit').addEventListener('click', resetForm);
+// Reset Form Input
+document.getElementById("handleSubmit").addEventListener("click", resetForm);
 
 function resetForm() {
   document.getElementById("myForm").reset();
 };
-
