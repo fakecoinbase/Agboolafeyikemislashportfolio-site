b89874826e39ddfb4e8e81dee1d6d7016314c155
Agboolafeyiemi <agboolafeyikemi93@gmail.com>
2020-03-11 18:20:03 +0100

    modify html file

--- dist/index.js
+++ dist/index.js
@@ -4,7 +4,7 @@ window.onload = addDynamicDate;
 function addDynamicDate() {
   const year = new Date().getFullYear();
   let node = document.createElement("LI");
-  let nodeText = document.createTextNode(` .© Copyright ${year}`);
+  let nodeText = document.createTextNode(` © Copyright ${year}`);
   node.appendChild(nodeText);
   document.getElementById("myList").appendChild(node);
 };
